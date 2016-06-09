3e4ea1227f357a4b34a15af20f1983d8eb22abaf
James McCray <chrisjamesjackmccray@gmail.com>
2016-06-09 00:22:09 -0500

    Commit

--- app/modules/whole/controllers/home.js
+++ app/modules/whole/controllers/home.js
@@ -1,7 +1,15 @@
 class HomeController {
   constructor() {
+    $(window).scroll(function(){
+      $("#opening-statement",).css("opacity", 1 - $(window).scrollTop() / 250);
+    });
 
+    $(window).scroll(function(){
+      $(".nav",).css("opacity", 1 - $(window).scrollTop() / 250);
+    });
   }
 }
+
+
 
 export default HomeController;
